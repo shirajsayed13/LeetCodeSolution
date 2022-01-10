8aa3c774b1fc848f8eefbf49ead665e47c426575
ShirajSayed <shirajsayed13@gmail.com>
2022-01-10 21:14:18 +0530

    Time: 93 ms (26.48%), Space: 54.1 MB (5.04%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length() > s2.length()) 
+            return false;
+        
+        int[] counter = new int[26];
+        for (char c : s1.toCharArray()) {
+            counter[c - 'a']++;
+        }
+        
+        String targetStr = Arrays.toString(counter);
+        int[] targetCounter = new int[26];
+        for(int i = 0; i< s1.length() - 1; i++) {
+            targetCounter[s2.charAt(i) - 'a']++;
+        }
+        int start = 0;
+        for(int i = s1.length() - 1; i < s2.length(); i++) {
+            targetCounter[s2.charAt(i) - 'a']++;
+            if(Arrays.toString(targetCounter).equals(targetStr)) {
+                return true;
+            }
+            targetCounter[s2.charAt(start++) - 'a']--;
+        }
+        return false;
+    }
+}
