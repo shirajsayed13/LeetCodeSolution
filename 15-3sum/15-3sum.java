724e7ecaf5a5c743588d8d07d2c416671f0bcb04
ShirajSayed <shirajsayed13@gmail.com>
2022-01-21 21:57:16 +0530

    Time: 27 ms (64.11%), Space: 56.8 MB (27.43%) - LeetHub

---
+++ 15-3sum/15-3sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < nums.length && nums[i] <= 0; i++) {
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSumHelper(nums, i, list);
+            }
+        }
+        return list;
+    }
+    
+    private void twoSumHelper(int[] nums, int i, List<List<Integer>> list) {
+        int low = i + 1;
+        int high = nums.length - 1;
+        while(low < high) {
+            int sum = nums[i] + nums[low] + nums[high];
+            if(sum < 0 || (low > i + 1 && nums[low] == nums[low - 1])) {
+                low++;
+            }
+            else if (sum > 0 || (high < nums.length - 1 && nums[high] == nums[high + 1])) {
+                high--;
+            } 
+            else {
+                list.add(Arrays.asList(nums[i], nums[low++], nums[high--]));
+            }
+        }
+    }
+}
