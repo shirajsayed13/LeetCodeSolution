c001d2dec3bb1c46c32c2c4cdd33299a16ad753c
ShirajSayed <shirajsayed13@gmail.com>
2022-10-17 07:29:19 +0530

    Time: 26 ms (34.69%), Space: 42.8 MB (71.79%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,25 +3,15 @@ class Solution {
         if(s.length() != t.length()) {
             return false;
         }
-        HashMap<Character, Character> map1 = new HashMap<>();
-        HashMap<Character, Boolean> map2 = new HashMap<>();
+        int[] map1 = new int[256];
+        int[] map2 = new int[256];
         
-        for(int i = 0; i < s.length(); i++) {
-            char ch1 = s.charAt(i);
-            char ch2 = t.charAt(i);
-            
-            if (map1.containsKey(ch1)) {
-                if(map1.get(ch1) != ch2) {
-                    return false;
-                } 
-            } else {
-                if (map2.containsKey(ch2)) {
-                    return false;
-                } else {
-                    map1.put(ch1, ch2);
-                    map2.put(ch2, true);
-                }
+        for (int idx = 0; idx < s.length(); idx++) {
+            if (map1[s.charAt(idx)] != map2[t.charAt(idx)]) {
+                return false;
             }
+            map1[s.charAt(idx)] = idx + 1;
+            map2[t.charAt(idx)] = idx + 1;
         }
         return true;
     }
